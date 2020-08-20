3b72bc49a21578126bf732b6f6b1c2deade330bd
Noah Gift <noah.gift@gmail.com>
2020-08-20 15:27:41 +0000

    adding requirements.txt

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
 install:
 	pip install --upgrade pip &&\
 		pip install -r requirements.txt
+
+format:
+	black hello.py
+
 lint:
 	pylint --disable=R,C hello.py
 
--- hello.py
+++ hello.py
@@ -1,14 +1,15 @@
 import click
 
-#var=
+# var=
 @click.command()
-@click.option('--name')
+@click.option("--name")
 def marco(name):
     if name == "Marco":
         click.echo("Polo")
     else:
         click.echo("No Match")
 
-if __name__ == '__main__':
-    #pylint: disable=no-value-for-parameter
+
+if __name__ == "__main__":
+    # pylint: disable=no-value-for-parameter
     marco()
